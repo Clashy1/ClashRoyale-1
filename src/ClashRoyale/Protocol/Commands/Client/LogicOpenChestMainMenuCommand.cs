ac6a046ffe954711774c703e6adefa12993df17e
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2020-07-31 19:20:33 +0300

    Add files via upload

---
+++ src/ClashRoyale/Protocol/Commands/Client/LogicOpenChestMainMenuCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ClashRoyale.Files;
+using ClashRoyale.Files.CsvLogic;
+using ClashRoyale.Logic;
+using ClashRoyale.Logic.Home.Chests.Items;
+using ClashRoyale.Protocol.Commands.Server;
+using ClashRoyale.Protocol.Messages.Server;
+using DotNetty.Buffers;
+
+namespace ClashRoyale.Protocol.Commands.Client
+{
+    public class LogicOpenChestMainMenuCommand : LogicCommand
+    {
+        public LogicOpenChestMainMenuCommand(Device device, IByteBuffer buffer) : base(device, buffer)
+        {
+        }
+
+        public override async void Process()
+        {
+            var home = Device.Player.Home;
+            await new AvailableServerCommand(Device)
+            {
+                Command = new ChestDataCommand(Device)
+                {
+                    Chest = home.Chests.BuyChest(1, Chest.ChestType.Shop)
+                }
+            }.SendAsync();
+        }
+    }
+}
